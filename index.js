12c2d7e760500333f370acefcfb8cee820365de7
simpart <simpart>
2018-10-14 15:36:36 +0000

    add comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * @file mofron-event-invclr/index.js
+ * @brief invert color event for mofron
  * @author simpart
  */
 const mf = require('mofron');
@@ -13,7 +14,7 @@ mf.event.Invclr = class extends mf.Event {
         try {
             super();
             this.name('Invclr');
-            this.prmMap('handler', 'colorTgt', 'value');
+            this.prmMap(['handler', 'targetColor']);
             this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
@@ -21,6 +22,11 @@ mf.event.Invclr = class extends mf.Event {
         }
     }
     
+    /**
+     * set invert listener
+     *
+     * @note private method
+     */
     component (prm) {
         try {
             let ret = super.component(prm);
@@ -28,20 +34,20 @@ mf.event.Invclr = class extends mf.Event {
                 let style = prm.styleTgt();
                 let fnc   = (p1, p2, p3) => {
                     try {
-                        let clr = mf.func.getColor(p1[p3.colorTgt()]);
+                        let clr = mf.func.getColor(p1[p3.targetColor()]);
                         if (null === clr) {
                             return;
                         }
                         let rgb = clr.rgb();
                         if (p3.value() > (rgb[0]+ rgb[1] + rgb[2])) {
-                            if (true !== p3.status()) {
+                            if (true !== p3.invStatus()) {
                                 p3.execHandler(true);
-                                p3.status(true);
+                                p3.invStatus(true);
                             }
                         } else {
-                            if (false !== p3.status()) {
+                            if (false !== p3.invStatus()) {
                                 p3.execHandler(false);
-                                p3.status(false);
+                                p3.invStatus(false);
                             }
                         }
                     } catch (e) {
@@ -49,7 +55,7 @@ mf.event.Invclr = class extends mf.Event {
                         throw e;
                     }
                 }
-                style.styleListener(this.colorTgt(), fnc, this);
+                style.styleListener(this.targetColor(), fnc, this);
             }
             return ret;
         } catch (e) {
@@ -60,51 +66,59 @@ mf.event.Invclr = class extends mf.Event {
     
     contents () {}
     
-    colorTgt (prm) {
+    /**
+     * setter/getter target color
+     * it trigger event when target color inverts
+     * 
+     * @param prm (string) css color key, default key is 'background'
+     * @param prm (undefined) call as getter
+     * @return (string) css color key
+     */
+    targetColor (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_clrtgt) ? 'background' : this.m_clrtgt;
-            }
-            /* setter */
-            if ('string' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_clrtgt = prm;
+            return this.member('targetColor', 'string', prm, 'background');
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter invert limit value 
+     * this value is used for invert judgment
+     * 
+     * @param prm (number) sum of rgb
+     * @param prm (undefined) call as getter
+     * @return (number) sum of rgb
+     */
     value (prm) {
-        try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_value) ? 390 : this.m_value;
-            }
-            /* setter */
-            if ('number' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_value = prm;
-        } catch (e) {
+        try { return this.member('value', 'number', prm, 390); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    status (prm) {
-        try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_status) ? null : this.m_status;
-            }
-            if ('boolean' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_status = prm;
-        } catch (e) {
+    /**
+     * setter/getter invert status
+     * 
+     * @param prm (true) inverted color
+     * @param prm (false) not inverted color
+     * @param prm (undefined) call as getter
+     * @return (boolean) invert status
+     */
+    invStatus (prm) {
+        try { return this.member('invStatus', 'boolean', prm, false); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * kick effect
+     * @note private method
+     */
+    execEffect (eff, prm) {
+        try { eff.execute(prm[0]); } catch (e) {
             console.error(e.stack);
             throw e;
         }
